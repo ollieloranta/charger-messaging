bcc58e5663fe75541c2bf2ddc15a084d58660d18
Olli Eloranta <olli.eloranta@neovision.fr>
2023-08-20 15:45:16 +0200

    [DEV] Add limit to msg sending.
    Just in case service is forgotten on.

--- charger_messaging/mock_sender.py
+++ charger_messaging/mock_sender.py
@@ -8,11 +8,16 @@ from charger_messaging.config import MQTT_URL, MQTT_PORT
 
 TOPIC = "charger/1/connector/1/session/1"
 SLEEP_TIME = 60
+MAX_TIME = 601
 
 
 def main():
     message_number = 1
+    start_time = time.time()
     while True:
+        if time.time() - start_time > MAX_TIME:
+            print("Stopping message sending.")
+            return
         payload = {"session_id": message_number, "delivered_kWh": 30, "duration_s": 45, "cost_cents": 70}
         publish.single(TOPIC, json.dumps(payload), hostname=MQTT_URL, port=int(MQTT_PORT))
         time.sleep(SLEEP_TIME)
